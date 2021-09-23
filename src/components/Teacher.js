776f5997537df81a7a3af677e630e0a3c5558e80
Ridhim06 <ridhimgarg@gmail.com>
2021-09-23 14:38:48 +0530

    yo

--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 function Teacher() {
     const [coursesList, setCourseslist] = useState(null);
     const getCourses = async () => {
-        let courses = await fetch('http://localhost:8000/getCourses').then(res => res.json()).then(data => {console.log('data', data);
+        let courses = await fetch('https://navigus-bquiz.herokuapp.com/getCourses').then(res => res.json()).then(data => {console.log('data', data);
         setCourseslist(data)}
         )
         // console.log("courses",courses)
